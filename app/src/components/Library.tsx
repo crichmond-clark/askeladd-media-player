4246d0407f8239792784b816271c16f910976946
Connor Richmond-Clark <crichmondclark@gmail.com>
2023-11-13 04:47:05 +0000

    remove duplicate songs from library

--- app/src/components/Library.tsx
+++ app/src/components/Library.tsx
@@ -11,11 +11,32 @@ export function Library() {
     parent.current && autoAnimate(parent.current);
   }, [parent]);
 
+  const uniqueSongs = [
+    ...new Set(
+      songs.map((song) =>
+        JSON.stringify({
+          title: song.title,
+          artist: song.artist,
+          album: song.album,
+        }),
+      ),
+    ),
+  ].map((songStr) =>
+    songs.find(
+      (song) =>
+        JSON.stringify({
+          title: song.title,
+          artist: song.artist,
+          album: song.album,
+        }) === songStr,
+    ),
+  );
+
   return (
     <div className="mx-2 my-12 grid place-items-center" ref={parent}>
-      {songs.map((song, index) => (
+      {uniqueSongs.map((song, index) => (
         <div key={index} className="mb-2">
-          <Song song={song} index={index} />
+          {song ? <Song song={song} index={index} /> : null}
         </div>
       ))}
     </div>
